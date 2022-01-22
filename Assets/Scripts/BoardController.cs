6c6034c73d040ef4cf1a3590e73269dc08487749
Júlio Barros <julio.barros@movile.com>
2022-01-22 16:25:05 -0300

    procedural tiles

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    [SerializeField] private GameObject tile;
+    [SerializeField] private TileMovementSystem movementSystem;
+    private TileController[][] _tileArray;
+    void Start()
+    {
+        _tileArray = new TileController[8][];
+        for (int x = 0; x < 8; x++)
+        {
+            _tileArray[x] = new TileController[8];
+            for (int z = 0; z < 8; z++)
+            {
+                var posX = x * 1.8f + (z % 2 == 0 ? 0f : 0.9f); 
+                var currentTile = 
+                    Instantiate(tile, new Vector3(posX, 0, z*1.57f), Quaternion.identity, transform);
+                currentTile.name = $"tile ({x}, {z})";
+                var tileController = currentTile.GetComponentInChildren<TileController>();
+                _tileArray[x][z] = tileController;
+                tileController.RegisterMovementSystem(movementSystem);
+            }
+        }
+    }
+}
--- Assets/Scripts/TileMovementSystem.cs
+++ Assets/Scripts/TileMovementSystem.cs
@@ -8,23 +8,6 @@ using UnityEngine.WSA;
 public class TileMovementSystem : MonoBehaviour
 {
     [SerializeField] private CharacterController characterController;
-    [SerializeField] private GameObject board;
-    private TileController[][] _tileArray;
-
-    private void Start()
-    {
-        _tileArray = new TileController[100][];
-        var tiles = board.GetComponentsInChildren<TileController>();
-        foreach (var tile in tiles)
-        {
-            var position = tile.transform.position;
-            var x = Mathf.RoundToInt(position.x / 0.9f);
-            var z = Mathf.RoundToInt(position.z / 1.57f);
-            _tileArray[x] = new TileController[100];
-            _tileArray[x][z] = tile;
-            tile.RegisterMovementSystem(this);
-        }
-    }
 
     public void OnMoveToTile(TileController tileController)
     {
